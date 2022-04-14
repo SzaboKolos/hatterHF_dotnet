72a764fd22950ab90210835a52e9592cee66167c
SzaboKolos <kolor1400@gmail.com>
2022-04-14 21:49:40 +0200

    Compliance

--- Bme.Aut.Logistics/Model/Compliance.cs
+++ Bme.Aut.Logistics/Model/Compliance.cs
@@ -5,5 +5,8 @@
         public long Id { get; set; }
         public string InvoiceID { get; set; }
         public bool IsDone { get; set; }
+
+        public Section Section { get; set; }
+        public Partner Partner { get; set; }    
     }
 }
